bcba70af63ffc7f62f70e2a850ec4ebeee311482
SuruchiParashar <65654528+SuruchiParashar@users.noreply.github.com>
2020-06-16 15:21:10 +0530

    Add files via upload

---
+++ RhymeBank1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class RhymeBank1 {
+
+	public static void main(String[] args) {
+		System.out.println("Bank Admin Menu:");
+		
+	}
+	//Please refer to the other window to see the design
+}
